ddc35e2fe1012795b9953b0d26d7e53faa98e43d
David Nádvorník <david@nadvornik.cz>
2025-01-17 23:43:38 +0100

    fchange apui url env var to svelte variant

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,10 @@
 import PocketBase, { type RecordModel } from 'pocketbase';
-const apiUrl = import.meta.env.VITE_PUBLIC_API_URL; // Access public variable
-console.log('API URL:', apiUrl);
-const pb = new PocketBase(apiUrl); // Replace with your PocketBase URL
+import { env } from '$env/dynamic/public';
+
+const PUBLIC_API_URL = env.PUBLIC_API_URL;
+
+console.log('API URL:', PUBLIC_API_URL);
+const pb = new PocketBase(PUBLIC_API_URL); // Replace with your PocketBase URL
 pb.autoCancellation(false);
 
 export async function subscribeToAuctionUpdate(recordId: string, callback: (record: RecordModel) => void) {
